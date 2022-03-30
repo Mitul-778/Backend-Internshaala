8c5c783e690c4d42e4e6bfc8035d805c3d147b29
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-03-30 20:54:37 +0530

    training api and crud

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const trainingController = require("./controllers/training.controller")
 
 app.use(express.json());
+
+app.use(cors())
+
+app.use("/training",trainingController)
 
 module.exports=app;
--- src/public/scripts/training.js
+++ src/public/scripts/training.js
@@ -1,20 +1,66 @@
-const selected = document.querySelector(".selected")
-const optionsContainer = document.querySelector(".options_container")
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options_container");
 
-const optionList = document.querySelectorAll(".option")
+const optionList = document.querySelectorAll(".option");
 
-selected.addEventListener("click",() =>{
-    optionsContainer.classList.toggle("active")
-})
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
 
-optionList.forEach(ele =>{
-    ele.addEventListener("click", () =>{
-        selected.innerHTML = ele.querySelector("label").innerHTML
-        optionsContainer.classList.remove("active")
-    })
-})
+optionList.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    selected.innerHTML = ele.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
 
+const trainingData = async () => {
+  let res = await fetch("http://localhost:5000/training");
+  let data = await res.json();
+  appendData(data);
+};
+trainingData();
 
-import footer from "../scripts/footer.js"
+const appendData = (data) => {
+  data.map((el) => {
+    let div1 = document.createElement("div");
+    div1.setAttribute("class", "horizontal-items");
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "cource-title");
+    let div3 = document.createElement("div");
+    div3.setAttribute("class", "cource-desc");
+    let div4 = document.createElement("div");
+    div4.setAttribute("class", "cource-duration");
 
-document.querySelector("footer").innerHTML=footer()
+    let img = document.createElement("img");
+    img.src = el.image;
+    img.setAttribute("class", "cource-image");
+
+    let title = document.createElement("p");
+    title.textContent = el.title;
+
+    let description = document.createElement("p");
+    description.textContent = el.description;
+
+    let p1 = document.createElement("p");
+    p1.setAttribute("class", "cource-time");
+    p1.textContent = "Duration: 6 weeks";
+
+    let p2 = document.createElement("p");
+    p2.setAttribute("class", "know-more");
+    p2.textContent = "Know More";
+    p2.addEventListener("click", function () {
+      document.location.href = "./../html/web_development.html";
+    });
+
+    div2.append(title);
+    div3.append(description);
+    div4.append(p1, p2);
+    div1.append(img, div2, div3, div4);
+    document.querySelector(".horizontal-container").append(div1);
+  });
+};
+
+import footer from "../scripts/footer.js";
+
+document.querySelector("footer").innerHTML = footer();
