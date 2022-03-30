60dc5c21986f6298d18f3dd18270a60796a49f28
Suraj <rauniyar.suraj731@gmail.com>
2022-03-31 01:49:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const trainingController = require("./controllers/training.controller")
+const cardController =require("./controllers/card.controller")
 
 app.use(express.json());
 
 app.use(cors())
 
 app.use("/training",trainingController)
+app.use("/card",cardController);
 
 module.exports=app;
