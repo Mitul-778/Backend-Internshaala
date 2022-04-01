ce6133d358d7faca49fe4c4e71b7c2b26dbc34a1
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-04-01 19:52:16 +0530

    bug fixed in register

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const cors = require("cors");
+app.use(cors())
 const trainingController = require("./controllers/training.controller")
 const cardController =require("./controllers/card.controller")
 const {
@@ -10,21 +12,13 @@ const {
 } = require("./controllers/auth.controller");
 const userController = require("./controllers/user.controller")
 const passport = require("../src/configs/google.OAuth")
-const path =  require("path");
 
-app.use("/register", register)
-app.use("/login", login)
-app.use("/users", userController)
-// const static_path = path.join(__dirname, "/html")
-
-// app.use(express.static(static_path));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-app.use(cors())
 
 app.use("/training",trainingController)
 app.use("/card",cardController);
+app.use("/register", register)
+app.use("/login", login)
+app.use("/users", userController)
 
 app.get(
     "/auth/google",
